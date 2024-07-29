7599b522cb3092293bd7d1917e05e0eaa629be92
Mehreen Farooq <65364225+Mehreen66@users.noreply.github.com>
2024-07-29 11:36:56 +0500

    Time: 503 ms (5.19%), Space: 21.4 MB (92.38%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        l = 0
+        r = 1
+        while r < len(nums):
+            if nums[l] == nums[r]:
+                return True
+            l += 1
+            r += 1
+        return False
